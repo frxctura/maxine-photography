cf3507d36e12af2b2c240b92e6c17b8968474ed4
Maxine <chaxprivate@gmail.com>
2024-05-28 22:43:15 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,29 +1,51 @@
 import os
 import re
+import logging
 
-def rename_images(directory):
-    extensions = ['.jpg', '.jpeg', '.png', '.gif']
-    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    image_files = [f for f in files if any(f.lower().endswith(ext) for ext in extensions)]
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def get_next_available_number(existing_numbers):
+    num = 1
+    while num in existing_numbers:
+        num += 1
+    return num
+
+def rename_files(directory):
+    pattern = re.compile(r'image(\d+)\.jpg')
+    existing_numbers = set()
+    other_files = []
     
-    pattern = re.compile(r'^image\d+\.\w+$', re.IGNORECASE)
+    for filename in os.listdir(directory):
+        match = pattern.match(filename)
+        if match:
+            number = int(match.group(1))
+            existing_numbers.add(number)
+        else:
+            other_files.append(filename)
 
-    existing_numbers = []
-    for f in image_files:
-        if pattern.match(f):
-            existing_numbers.append(int(re.search(r'\d+', f).group()))
+    existing_numbers = sorted(existing_numbers)
 
-    max_number = max(existing_numbers) if existing_numbers else 0
+    for filename in other_files:
+        old_path = os.path.join(directory, filename)
+        new_number = get_next_available_number(existing_numbers)
+        new_filename = f'image{new_number}.jpg'
+        new_path = os.path.join(directory, new_filename)
+        
+        try:
+            os.rename(old_path, new_path)
+            existing_numbers.append(new_number)
+            existing_numbers = sorted(existing_numbers)
+            logging.info(f'Renamed {old_path} to {new_path}')
+        except Exception as e:
+            logging.error(f'Error renaming {old_path} to {new_path}: {e}')
 
-    for i, filename in enumerate(image_files, start=max_number + 1):
-        if pattern.match(filename):
-            continue 
-        ext = os.path.splitext(filename)[1]
-        new_filename = f'image{i}{ext}'
-        src = os.path.join(directory, filename)
-        dst = os.path.join(directory, new_filename)
-        os.rename(src, dst)
-        print(f'Renamed {src} to {dst}')
+def main():
+    directory = './photos'
+    if not os.path.exists(directory):
+        logging.error(f'Directory {directory} does not exist.')
+        return
 
-image_directory = './photos'  
-rename_images(image_directory)
+    rename_files(directory)
+
+if __name__ == '__main__':
+    main()
