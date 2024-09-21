305c2ca033aa02d8562178b1e33cd00319aca908
Maxine <chaxprivate@gmail.com>
2024-09-21 13:03:13 +0200

    Update gen.py

--- gen.py
+++ gen.py
@@ -16,11 +16,15 @@ def generate_thumbnails_and_json(image_directory, thumbnail_directory, json_file
                 thumbnail_image_path = os.path.join(thumbnail_directory, file)
                 
                 with Image.open(full_image_path) as img:
-                    img.thumbnail((512, 512))  
-                    img.save(thumbnail_image_path)
-                
-                full_image_url = '/photos/' + os.path.relpath(full_image_path, image_directory)
-                thumbnail_image_url = '/thumbnails/' + os.path.relpath(thumbnail_image_path, thumbnail_directory)
+                    img.thumbnail((512, 512))
+                    
+                    if img.mode in ('RGBA', 'P'):
+                        img = img.convert('RGB')
+                    
+                    img.save(thumbnail_image_path, format='JPEG') 
+
+                full_image_url = '/photos/' + os.path.relpath(full_image_path, image_directory).replace('\\', '/')
+                thumbnail_image_url = '/thumbnails/' + os.path.relpath(thumbnail_image_path, thumbnail_directory).replace('\\', '/')
                 
                 images_data.append({
                     'thumbnail': thumbnail_image_url,
@@ -28,6 +32,7 @@ def generate_thumbnails_and_json(image_directory, thumbnail_directory, json_file
                 })
 
     images_data.sort(key=lambda x: x['fullSize']) 
+    
     with open(json_file_path, 'w') as json_file:
         json.dump({'images': images_data}, json_file, indent=2)
 
